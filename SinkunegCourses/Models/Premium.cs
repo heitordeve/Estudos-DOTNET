8d4b898c7f6799f8be2fbccf4f2caa6ed1fbb732
heitordeve <heitor.developer@outlook.com>
2025-04-27 13:21:20 -0300

    Criando Models de Student e Premium
    Signed-off-by: heitordeve <heitor.developer@outlook.com>

---
+++ SinkunegCourses/Models/Premium.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SinkunegCourses.Models;
+
+public class Premium
+{
+    [Key]
+    [Display(Name = "Premium ID")]
+    public int PremiumID { get; set; }
+    
+    [Required(ErrorMessage = "Informe o título do Premium")]
+    [StringLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O título não pode ter menos que 5 caracteres")]
+    [Display(Name = "Título")]
+    public string Titulo { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    [DisplayName("Inicio")]
+    public DateTime Inicio { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    [DisplayName("Termino")]
+    public DateTime Termino { get; set; }
+    
+    [Display(Name = "Aluno")]
+    [Required(ErrorMessage = "Aluno Inválido")]
+    public string StudentId{ get; set; }
+
+    public Student? Student { get; set; }
+}
---
+++ SinkunegCourses/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SinkunegCourses.Models;
+
+public class Student
+{
+    [Key]
+    [Display(Name = "Student ID")]
+    public int StudentID { get; set; }
+    
+    [Required(ErrorMessage = "Informe o nome do estudante")]
+    [StringLength(80, ErrorMessage = "O nome do estudante de ter até 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O nome não pode ter menos que 5 caracteres")]
+    [Display(Name = "Nome completo")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Informe o e-mail do estudante")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    [Display(Name = "E-mail")]
+    public string Email { get; set; }
+
+    public List<Premium> Premiums { get; set; } = new();
+
+}
